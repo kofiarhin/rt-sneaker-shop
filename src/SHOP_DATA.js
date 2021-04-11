6b79945604a685ca30630a4d9a0a2537d19cda86
mac <mac@esco-3.local>
2021-04-11 03:38:20 +0000

    updated shop data, restructured the id's

--- src/SHOP_DATA.js
+++ src/SHOP_DATA.js
@@ -1,6 +1,10 @@
 // todo --add sizies to data
 
+// 31 items
+
 const SHOP_DATA = [
+
+    // JORDAN
         {
             id: 1,
             title: "jordan",
@@ -35,27 +39,27 @@ const SHOP_DATA = [
                 },
 
                  {
-                    id:26,
+                    id:5,
                     name: "AIR JORDAN 4 RETRO 'ORANGE METALLIC'",
                     price: 404,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
                 {
-                    id:27,
+                    id:6,
                     name: "AIR JORDAN 4 RETRO 'TAUPE HAZE'",
                     price: 379.99,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:28,
+                    id:7,
                     name: "AIR JORDAN 4 WMNS RETRO 'STARFISH'",
                     price: 379.99,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:29,
+                    id:8,
                     name: "AIR JORDAN 1 ZOOM COMFORT 'LONDON'",
                     price: 219.99,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
@@ -65,42 +69,45 @@ const SHOP_DATA = [
             ]
         },
 
-        // addidas
+
+
+
+        // ADIDAS
 
          {
             id: 2,
             title: "adidas",
             items: [
                 {
-                    id:5,
+                    id:9,
                     name: "ADIDAS YEEZY 700 V3 'CLAY BROWN'",
                     price: 400,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:6,
+                    id:10,
                     name: "ADIDAS YEEZY BOOST 350 V2 'YEEZREEL NON-REFLECTIVE'",
                     price: 450,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:7,
+                    id:11,
                     name: "ADIDAS YEEZY BOOST 350 V2 CLAY",
                     price: 300,
                     description: " to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:8,
+                    id:12,
                     name: "ADIDAS YEEZY BOOST 700 ‘SUN'",
                     price: 400,
                     description: "content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                                 {
-                    id:9,
+                    id:13,
                     name: "ADIDAS YEEZY 700 V3 'KYANITE'",
                     price: 350,
                     description: "content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
@@ -108,7 +115,7 @@ const SHOP_DATA = [
 
 
                                {
-                    id:29,
+                    id:14,
                     name: "ADIDAS YEEZY BOOST 380 'CALCITE GLOW'",
                     price: 279.99,
                     description: "content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
@@ -118,69 +125,80 @@ const SHOP_DATA = [
 
 
 
+        // NIKE 
+
         {
             id: 3,
             title: "nike",
             items: [
                 {
-                    id:10,
+                    id:15,
                     name: "GRATEFUL DEAD X NIKE DUNK LOW SB 'ORANGE BEAR'",
                     price: 400,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 },
 
                 {
-                    id:11,
+                    id:16,
                     name: "NIKE DUNK HIGH SP 'MICHIGAN' 2020",
                     price: 450,
                     description: "content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their "
                 },
 
                 {
-                    id:12,
+                    id:17,
                     name: "NIKE DUNK LOW SB 'NIGHT OF MISCHIEF'",
                     price: 300,
                     description: "some details about product"
                 },
 
                 {
-                    id:13,
+                    id:18,
                     name: "NIKE DUNK LOW SP RETRO 'VENEER' 2020",
                     price: 400,
                     description: "some details about product"
+                },
+
+
+                {
+                    id:19,
+                    name: "DQM X NIKE AIR MAX 90 'BACON' 2021",
+                    price: 209.99,
+                    description: "some details about product",
+                    sizes: [32, 38, 45]
                 },
             ]
         },
 
 
-        // new arrivals
+        // NEW ARRIVAL
         {
-            id: 4,
+            id: 3,
             title: "new arrivals",
             items: [
                 {
-                    id:14,
+                    id:20,
                     name: "ADIDAS YEEZY BOOST 350 V2 'YEEZREEL NON-REFLECTIVE'",
                     price: 400,
                     description: "some details about product"
                 },
 
                 {
-                    id:15,
+                    id:21,
                     name: "ADIDAS YEEZY BOOST 350 V2 CLAY",
                     price: 450,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 },
 
                 {
-                    id:16,
+                    id:22,
                     name: "AIR JORDAN 1 MID PS 'HYPER ROYAL'",
                     price: 300,
                     description: "dney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the "
                 },
 
                 {
-                    id:17,
+                    id:13,
                     name: "AIR JORDAN 1 MID SE 'TURF ORANGE'",
                     price: 400,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
@@ -189,20 +207,59 @@ const SHOP_DATA = [
         },
 
 
+     
+
+         // WOMEN
+        {
+            id: 4,
+            title: "women",
+            items: [
+                {
+                    id:28,
+                    name: "AIR JORDAN 1 WMNS LOW SE 'DARK BEETROOT'",
+                    price: 400,
+                    description: "some details about product"
+                },
+
+                {
+                    id:29,
+                    name: "AIR JORDAN 1 WMNS MID 'SIREN RED' ",
+                    price: 450,
+                    description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+                },
+
+                {
+                    id:30,
+                    name: "AIR JORDAN 1 WMNS MID SE 'PEACH MOCHA'",
+                    price: 300,
+                    description: "dney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the "
+                },
+
+                {
+                    id:31,
+                    name: "AIR JORDAN 1 WMNS RETRO HIGH OG 'SILVER TOE'",
+                    price: 400,
+                    description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
+                },
+            ]
+        },
+
+
+
         //kids
         {
             id: 5,
             title: "kids",
             items: [
                 {
-                    id:18,
+                    id:24,
                     name: "AIR JORDAN 1 MID SE PS 'VARSITY PURPLE'",
                     price: 400,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 },
 
                 {
-                    id:19,
+                    id:25,
                     name: "ADIDAS YEEZY BOOST 350 V2 KIDS 'LUNDMARK NON-REFLECTIVE'",
                     price: 400,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
@@ -210,14 +267,14 @@ const SHOP_DATA = [
 
 
                 {
-                    id:20,
+                    id:26,
                     name: "ADIDAS YEEZY BOOST 350 V2 KIDS 'YECHEIL'",
                     price: 430,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
                 },
 
                 {
-                    id:21,
+                    id:27,
                     name: "NIKE DUNK LOW PS 'ORANGE PEARL'",
                     price: 430,
                     description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
@@ -228,45 +285,12 @@ const SHOP_DATA = [
             ]
         },
 
-
-         // women
-        {
-            id: 4,
-            title: "women",
-            items: [
-                {
-                    id:22,
-                    name: "AIR JORDAN 1 WMNS LOW SE 'DARK BEETROOT'",
-                    price: 400,
-                    description: "some details about product"
-                },
-
-                {
-                    id:23,
-                    name: "AIR JORDAN 1 WMNS MID 'SIREN RED' ",
-                    price: 450,
-                    description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
-                },
-
-                {
-                    id:24,
-                    name: "AIR JORDAN 1 WMNS MID SE 'PEACH MOCHA'",
-                    price: 300,
-                    description: "dney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the "
-                },
-
-                {
-                    id:25,
-                    name: "AIR JORDAN 1 WMNS RETRO HIGH OG 'SILVER TOE'",
-                    price: 400,
-                    description: " have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
-                },
-            ]
-        },
+        // END KIDS
 
 
 
 
 ]
+
 
 export default SHOP_DATA
